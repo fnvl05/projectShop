6ac0547344e35e452caf7b9be29cbaa9b127fcba
kang <roto521@naver.com>
2020-03-17 20:07:52 +0900

    수정

--- test_project/src/main/java/com/test/project01/cart/controller/CartController.java
+++ test_project/src/main/java/com/test/project01/cart/controller/CartController.java
@@ -5,13 +5,13 @@ package com.test.project01.cart.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.test.project01.cart.dto.CartListDto;
 import com.test.project01.cart.service.CartService;
