7cd0b8c0c25dc0e059e9949ca97f956e227c5c7f
hyunseunghwan <hyunsh1228@naver.com>
2020-04-02 09:50:11 +0900

    table2.sql에 qna,notice 테이블 정리

--- test_project/src/main/webapp/WEB-INF/table2.sql
+++ test_project/src/main/webapp/WEB-INF/table2.sql
@@ -1,3 +1,42 @@
--- 유저
+<!-- 공지 테이블 -->
+CREATE TABLE board_notice(
+	num Number PRIMARY key,
+	writer VARCHAR2(100) not null, -- 글 작성자의 id
+	title VARCHAR2(100) not null,
+	content CLOB,
+	viewCount NUMBER, -- 조회수
+	regdate DATE
+);
 
--- 위시리스트
+alter table board_notice add(noticeNum number not null); 
+
+CREATE SEQUENCE board_notice_seq;
+
+
+<!-- QnA 테이블 -->
+CREATE TABLE board_qna(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100) NOT NULL, -- 글 작성자의 id 
+	title VARCHAR2(100) NOT NULL,
+	content CLOB,
+	viewCount NUMBER, -- 조회수
+	regdate DATE
+);
+alter table board_qna add(itemNum number);
+
+CREATE SEQUENCE board_qna_seq;
+
+
+<!-- QnA 게시판 댓글 -->
+CREATE TABLE board_qna_comment(
+	num NUMBER PRIMARY KEY, -- 댓글의 글번호
+	writer VARCHAR2(100), -- 댓글 작성자
+	content VARCHAR2(500), -- 댓글 내용
+	target_id VARCHAR2(100), -- 댓글의대상이되는아이디(글작성자)
+	ref_group NUMBER, -- 댓글 그룹번호
+	comment_group NUMBER, -- 원글에 달린 댓글 내에서의 그룹번호
+	deleted CHAR(3) DEFAULT 'no', -- 댓글이 삭제 되었는지 여부 
+	regdate DATE -- 댓글 등록일 
+);
+
+CREATE SEQUENCE board_qna_comment_seq;
